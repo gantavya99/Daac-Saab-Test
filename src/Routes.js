52708f5ed9cb5651516dd4bb3e70be5667e70cbb
gantavya99 <65835480+gantavya99@users.noreply.github.com>
2022-12-23 19:04:58 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/Routes.js
@@ -0,0 +1,19 @@
+import React, { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import Homepage from "../src/views/Homepage/js/Homepage";
+import OneTap from "./views/One-tap/js/OneTap";
+import DoctorPage from "./views/DoctorPage/js/DoctorPage.js";
+import AboutUs from "./views/About-us/js/AboutUs";
+
+export default function DaacSaabRoutes() {
+  return (
+    <Suspense fallback={<div></div>}>
+      <Routes>
+        <Route exact path="/home" element={<Homepage />} />
+        <Route path="/One-tap" element={<OneTap />} />
+        <Route path="/Doctor" element={<DoctorPage />} />
+        <Route path="/About-us" element={<AboutUs />} />
+      </Routes>
+    </Suspense>
+  );
+}
